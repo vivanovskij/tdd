4e8f33c4249dd3abe877909c99cc8a52f5a1d8e8
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-10-14 18:43:22 +0300

    first MT and converting URL fictitious view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
     
-    def test_bad_maths(self):
-        '''тест: неправильные мат. расчеты'''
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой url преобразуется в представление homepage'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+

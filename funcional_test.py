85f3d526cae971a016d57b54f7a8e97ab4919c2e
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-10-14 16:44:36 +0300

    extended first FT in comments, now uses unittest

--- funcional_test.py
+++ funcional_test.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    '''Тест нового посетителя'''
 
-assert 'Django' in browser.title
+    def setUp(self):
+        '''Установка'''
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        '''демонтаж'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''тест: можно начать список и получить его позже'''
+        # Открыть домашнюю страницу
+        self.browser.get('http://localhost:8000')
+
+        # Заголовок и шапка
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест')
+
+        # Ввести элемент списка
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

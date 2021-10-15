c6f9bd04094d2463d5546b8620faaf19b10cab32
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-10-15 21:30:32 +0300

    ФТ теперь проверяет возможность ввести элемент списка

---
+++ functional_test.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    '''Тест нового посетителя'''
+
+    def setUp(self):
+        '''Установка'''
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        '''демонтаж'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''тест: можно начать список и получить его позже'''
+        # Открыть домашнюю страницу
+        self.browser.get('http://localhost:8000')
+
+        # Заголовок и шапка
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attrubute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Набрать в текстовом поле "Купить павлиньи перья"
+        inputbox.send_keys('Купить павлиньи перья')
+
+        # Нажать Enter, страница обновляется
+        # теперь страница содержит "1: Купить павлиньи перья"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+
+        self.fail('Закончить тест')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
